400ceba61045395f8a707031d18de79fa1590aab
meriem meriem <chamimeriem2302@gmail.com>
2024-10-01 15:22:57 +0100

    navbar text

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -6,12 +6,13 @@ const NavBar = () => {
     <div
       className={`flex justify-between items-center w-full h-20 px-4 md:px-20 bg-second fixed z-10`}
     >
-      <div>
+      <div className="flex">
         <h1
           className={`coffee_name  text-3xl md:text-4xl text-primary `}
         >
           BETWEEN US
         </h1>
+        <p className="menu_name">Coffee</p>
       </div>
     </div>
   );
